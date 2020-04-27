899898b8f877959a0fffe38d893c14cb2c38da26
bharathjinka09 <bharathjinka09@gmail.com>
2020-04-27 13:05:17 +0530

    Updates

---
+++ precache-manifest.82d072fc1336e9cd193560dae01e2408.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea4ee59c00ebc8eaed895b598b93beb",
+    "url": "/bharath-todo/index.html"
+  },
+  {
+    "revision": "6cdf8b15e1a77c89c5ce",
+    "url": "/bharath-todo/static/css/main.4bcb7d6e.chunk.css"
+  },
+  {
+    "revision": "2753490be274190f97b0",
+    "url": "/bharath-todo/static/js/2.9bc38a1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bharath-todo/static/js/2.9bc38a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cdf8b15e1a77c89c5ce",
+    "url": "/bharath-todo/static/js/main.019c5414.chunk.js"
+  },
+  {
+    "revision": "9e714d9c10b84700e545",
+    "url": "/bharath-todo/static/js/runtime-main.e25c8306.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bharath-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
